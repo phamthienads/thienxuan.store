0ea74c957d564683e033590cc75b6081aee35bfa
Neil <phamthien.ads@icloud.com>
2022-12-20 03:02:54 +0700

    Update

---
+++ js/plugins.js
@@ -0,0 +1,29 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "108508995300544");
+    chatbox.setAttribute("attribution", "install_email");
+    chatbox.setAttribute("attribution_version", "biz_inbox");
+}());
+
+
+// Place any jQuery/helper plugins in here.
